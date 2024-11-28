d2904b3eedcb961cfaa5bf515ad79a06d8aa2c2e
kosi <kossyfab@gmail.com>
2024-11-28 22:29:00 +1100

    data ingestion component created

---
+++ main.py
@@ -0,0 +1,19 @@
+from network_security.components.data_ingestion import DataIngestion
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+import sys
+
+if __name__ == "__main__":
+    try:
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        
+        logging.info("Initiating Data Ingestion")
+
+        artifact = data_ingestion.initiate_data_ingestion()
+        print(artifact)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
