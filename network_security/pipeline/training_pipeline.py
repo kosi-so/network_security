931a9d047d21856ec2fbfc40c979b392b007b0af
kosi <kossyfab@gmail.com>
2024-11-30 22:03:21 +1100

    aws s3 bucket created

--- network_security/pipeline/training_pipeline.py
+++ network_security/pipeline/training_pipeline.py
@@ -25,7 +25,7 @@ from network_security.entity.artifact_entity import (
 )
 
 from network_security.constants.training_pipeline import TRAINING_BUCKET_NAME
-# from network_security.cloud.s3_syncer import S3Sync
+from network_security.cloud.s3_syncer import S3Sync
 from network_security.constants.training_pipeline import SAVED_MODEL_DIR
 import sys
 
@@ -33,7 +33,7 @@ import sys
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config=TrainingPipelineConfig()
-        # self.s3_sync = S3Sync()
+        self.s3_sync = S3Sync()
         
 
     def start_data_ingestion(self):
@@ -93,16 +93,17 @@ class TrainingPipeline:
     def sync_artifact_dir_to_s3(self):
         try:
             aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,
+                                           aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    ## local final model is going to s3 bucket 
-        
+    ## local final model is going to s3 bucket     
     def sync_saved_model_dir_to_s3(self):
         try:
             aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.model_dir,
+                                           aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
